1a702fa89e82261f6705d33d066510782b031f10
OptimistPro <aleksandrmalhal@gmail.com>
2022-02-16 21:10:18 +0700

    3.12

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import bridge from '@vkontakte/vk-bridge';
 bridge.send('VKWebAppInit');
+bridge.send("VKWebAppAllowNotifications");
+//bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
 
-bridge.send("VKWebAppCheckNativeAds", {"ad_format": "interstitial"});
+//bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
+//.then(data => console.log(data.result))
+//.catch(error => console.log(error));
 
-bridge.send("VKWebAppShowNativeAds", {ad_format:"interstitial"})
-.then(data => console.log(data.result))
-.catch(error => console.log(error));
